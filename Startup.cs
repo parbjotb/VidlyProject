27183f3c8f6284058617da95a87be7aed00e1050
parbjotb <72175682+parbjotb@users.noreply.github.com>
2020-10-05 16:27:15 -0500

    Project files

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyThree.Startup))]
+namespace VidlyThree
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
